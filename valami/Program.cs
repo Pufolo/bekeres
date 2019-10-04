8b00fff4e5379e80e0e5130acac42db0baa4318d
Petrik Lajos Szg <info@petrik.hu>
2019-10-04 12:09:49 +0200

    bekeres

--- valami/Program.cs
+++ valami/Program.cs
@@ -10,6 +10,16 @@ namespace valami
     {
         static void Main(string[] args)
         {
+            List<int> bekertSzamok = new List<int>();
+            //darabszám
+            Console.WriteLine("Hány darab elemet szeretne megadni?");
+            int elemSzam = Convert.ToInt32(Console.ReadLine());
+            //bekérés
+            for (int i = 0; i < elemSzam; i++)
+            {
+                Console.WriteLine("Kérem adjon meg egy számot!");
+                bekertSzamok.Add(Convert.ToInt32(Console.ReadLine()));
+            }
         }
     }
 }
